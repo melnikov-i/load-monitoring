f633bc514c7273ffa85446bea230473a0449c8ea
Мельников Иван Игоревич <melnikov-ivan@mail.ru>
2018-02-06 00:41:06 +0300

    beautiful-dnd test

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,4 +1,9 @@
 import * as React from 'react';
+import {
+  DragDropContext,
+  Droppable,
+  Draggable
+} from 'react-beautiful-dnd';
 
 import {
   DashboardInterface,
@@ -36,35 +41,89 @@ export const Dashboard: React.SFC<DashboardProps> = (props) => {
   }
   const Dashboard = getDashboard();
 
+  const onDragEnd = (result) => {
+    console.log('result:', result);
+  };
+
   console.log('Dashboard:', Dashboard);
   // console.log('ID', id);
 
   if ( Dashboard.dash_id.dashboard_id !== '' ) {
     return (
       <DashboardLayout>
+        <DragDropContext
+          onDragEnd={onDragEnd}
+        >
+          <Droppable droppableId={'test'}>
+            {(provided, snapshot) => {
+              console.log('provided:', provided);
+              console.log('snapshot:', snapshot);
+              return (
+                <div>
+                  <Draggable draggableId={'test-1'}>
+                    {(provided, snapshot) => {
+                      return (
+                        <div>
+                          <DashboardText>
+                            {'hello_1'}
+                          </DashboardText>
+                          {provided.placeholder}
+                        </div>
+                      )
+                    }}
+                  </Draggable>
+                  <Draggable draggableId={'test-2'}>
+                    {(provided, snapshot) => {
+                      return (
+                        <div>
+                          <DashboardText>
+                            {'hello_2'}
+                          </DashboardText>
+                          {provided.placeholder}
+                        </div>
+                      )
+                    }}
+                  </Draggable>
+                  <Draggable draggableId={'test-3'}>
+                    {(provided, snapshot) => {
+                      return (
+                        <div>
+                          <DashboardText>
+                            {'hello_3'}
+                          </DashboardText>
+                          {provided.placeholder}
+                        </div>
+                      )
+                    }}
+                  </Draggable>
+                  {provided.placeholder}
+                </div>
+              )
+            }}
+          </Droppable>
+        </DragDropContext>
+
         <DashboardText>
           {'Dashboard ID: ' + Dashboard.dash_id.dashboard_id}
         </DashboardText>
         <DashboardText>
           {'Название' + Dashboard.dash_id.dashboard_name}
         </DashboardText>
-  {
-    Dashboard.dash_data.map((e, i) => {
-      return (
-        <div key={i}>
-          <DashboardText>
-            {'Device ID:' + e.device_id }
-          </DashboardText>
-          <DashboardText>
-            {'Widget Name:' + e.widget_name }
-          </DashboardText>
-          <DashboardText>
-            {'Widget Width:' + e.widget_width }
-          </DashboardText>          
-        </div>
-      );
-    })
-  }
+          {Dashboard.dash_data.map((e, i) => {
+            return (
+              <div key={i}>
+                <DashboardText>
+                  {'Device ID:' + e.device_id }
+                </DashboardText>
+                <DashboardText>
+                  {'Widget Name:' + e.widget_name }
+                </DashboardText>
+                <DashboardText>
+                  {'Widget Width:' + e.widget_width }
+                </DashboardText>          
+              </div>
+            );
+          })}
       </DashboardLayout>
     );    
   } else {
