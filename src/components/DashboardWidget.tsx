d2105cef662b186a672dd0b91fab41230ab1944a
Ivan Melnikov <melnikov-ivan@mail.ru>
2018-02-28 22:50:31 +0300

    connectDragPreview in progress

--- src/components/DashboardWidget.tsx
+++ src/components/DashboardWidget.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as ReactDnd from 'react-dnd';
+import { getEmptyImage } from 'react-dnd-html5-backend';
 
 import {
   DashboardWidgetWrapperInterface,
@@ -22,9 +23,58 @@ export interface DashboardWidgetProps {
   isDragging?: boolean,
   connectDropTarget?: ReactDnd.ConnectDropTarget,
   connectDragSource?: ReactDnd.ConnectDragSource,
+  connectDragPreview?: ReactDnd.ConnectDragPreview,
 }
 
-export const DashboardWidget: 
+export class DashboardWidget extends React.Component<DashboardWidgetProps> {
+  componentDidMount() {
+    console.log('didMount');
+    const img = getEmptyImage();
+    const connectDragPreview = 
+      this.props.connectDragPreview ? this.props.connectDragPreview : null;
+    if ( connectDragPreview !== null ) {
+      img.onload = () => connectDragPreview(img);
+      console.log('img:', img);
+    }
+  }
+
+  render() {
+    const {
+      element,
+      isDragging,
+      connectDragSource,
+      connectDropTarget,
+    } = this.props;
+
+    if ( !connectDragSource || !connectDropTarget ) {
+      return null;
+    }
+
+    return connectDragSource(
+      connectDropTarget(
+        <div
+          className={'dashboardWidgetWrapper'}
+          style={{
+            width: getWidth(element.width),
+            marginRight: checkPosition(
+                Number(element.width),
+                element.index
+              ) ? ( element.width === '1' ) ? '0' : '2%' : '0',
+            marginBottom: ( element.width === '1' ) 
+              ? '20px' : '2%',
+            visibility: isDragging ? 'hidden' : 'visible',
+            // opacity: isDragging ? 1 : 1,
+            cursor: 'move',
+          }}
+        >
+
+        </div>
+      )
+    );
+  }
+}
+
+export const DashboardWidget1: 
 React.SFC<DashboardWidgetProps> = (props) => {
   const {
     element,
