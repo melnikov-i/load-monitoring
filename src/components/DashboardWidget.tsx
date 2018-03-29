f3fb299353e48ff96970a41a9600d624406f721d
Ivan Melnikov <melnikov-ivan@mail.ru>
2018-03-29 17:32:09 +0300

    Charts in progress

--- src/components/DashboardWidget.tsx
+++ src/components/DashboardWidget.tsx
@@ -7,6 +7,8 @@ import {
   // Dots,
   Bars,
   Ticks,
+  Handlers,
+  Title,
   // Transform,
   // Dots,
 } from 'rumble-charts';
@@ -43,125 +45,68 @@ const getColor = ( y: number ) => {
 
 const series: any = [
   {
-    data: [{y:90, x:0, color: getColor(90)},],
-  }, {
-    data: [{y:2, x:1, color: getColor(2)},],
-  }, {
-    data: [{y:4, x:2, color: getColor(4)},],
-  }, {
-    data: [{y:6, x:3, color: getColor(6)},],
-  }, {
-    data: [{y:8, x:4, color: getColor(8)},],
-  }, {
-    data: [{y:10, x:5, color: getColor(10)},],
-  }, {
-    data: [{y:12, x:6, color: getColor(12)},],
-  }, {
-    data: [{y:14, x:7, color: getColor(14)},],
-  }, {
-    data: [{y:16, x:8, color: getColor(16)},],
-  }, {
-    data: [{y:18, x:9, color: getColor(18)},],
-  }, {
-    data: [{y:20, x:10, color: getColor(20)},],
-  }, {
-    data: [{y:22, x:11, color: getColor(22)},],
-  }, {
-    data: [{y:24, x:12, color: getColor(24)},],
-  }, {
-    data: [{y:26, x:13, color: getColor(26)},],
-  }, {
-    data: [{y:28, x:14, color: getColor(28)},],
-  },{
-    data: [{y:30, x:15, color: getColor(30)},],
-  }, {
-    data: [{y:32, x:16, color: getColor(32)},],
-  },{
-    data: [{y:34, x:17, color: getColor(34)},],
-  }, {
-    data: [{y:36, x:18, color: getColor(36)},],
-  }, {
-    data: [{y:38, x:19, color: getColor(38)},],
-  }, {
-    data: [{y:40, x:20, color: getColor(40)},],
-  }, {
-    data: [{y:42, x:21, color: getColor(42)},],
-  }, {
-    data: [{y:44, x:22, color: getColor(44)},],
-  }, {
-    data: [{y:46, x:23, color: getColor(46)},],
-  }, {
-    data: [{y:48, x:24, color: getColor(48)},],
-  },{
-    data: [{y:50, x:25, color: getColor(50)},],
-  }, {
-    data: [{y:52, x:26, color: getColor(52)},],
-  },{
-    data: [{y:54, x:27, color: getColor(54)},],
-  }, {
-    data: [{y:56, x:28, color: getColor(56)},],
-  }, {
-    data: [{y:58, x:29, color: getColor(58)},],
-  },{
-    data: [{y:60, x:30, color: getColor(60)},],
-  }, {
-    data: [{y:62, x:31, color: getColor(62)},],
-  }, {
-    data: [{y:64, x:32, color: getColor(64)},],
-  }, {
-    data: [{y:66, x:33, color: getColor(66)},],
-  }, {
-    data: [{y:68, x:34, color: getColor(68)},],
-  }, {
-    data: [{y:70, x:35, color: getColor(70)},],
-  }, {
-    data: [{y:72, x:36, color: getColor(72)},],
-  }, {
-    data: [{y:74, x:37, color: getColor(74)},],
-  }, {
-    data: [{y:76, x:38, color: getColor(76)},],
-  }, {
-    data: [{y:78, x:39, color: getColor(78)},],
-  }, {
-    data: [{y:80, x:40, color: getColor(80)},],
-  }, {
-    data: [{y:82, x:41, color: getColor(82)},],
-  }, {
-    data: [{y:84, x:42, color: getColor(84)},],
-  }, {
-    data: [{y:86, x:43, color: getColor(86)},],
-  }, {
-    data: [{y:88, x:44, color: getColor(88)},],
-  },{
-    data: [{y:90, x:45, color: getColor(90)},],
-  }, {
-    data: [{y:92, x:46, color: getColor(92)},],
-  },{
-    data: [{y:94, x:47, color: getColor(94)},],
-  }, {
-    data: [{y:96, x:48, color: getColor(96)},],
-  }, {
-    data: [{y:98, x:49, color: getColor(98)},],
-  }, {
-    data: [{y:96, x:50, color: getColor(96)},],
-  }, {
-    data: [{y:94, x:51, color: getColor(94)},],
-  }, {
-    data: [{y:92, x:52, color: getColor(92)},],
-  }, {
-    data: [{y:90, x:53, color: getColor(90)},],
-  }, {
-    data: [{y:88, x:54, color: getColor(88)},],
-  },{
-    data: [{y:86, x:55, color: getColor(86)},],
-  }, {
-    data: [{y:84, x:56, color: getColor(84)},],
-  },{
-    data: [{y:82, x:57, color: getColor(82)},],
-  }, {
-    data: [{y:80, x:58, color: getColor(80)},],
-  }, {
-    data: [{y:78, x:59, color: getColor(78)},],
+    data: [
+      {y:90, x:0, color: getColor(90)},
+      {y:2, x:1, color: getColor(2)},
+      {y:4, x:2, color: getColor(4)},
+      {y:6, x:3, color: getColor(6)},
+      {y:8, x:4, color: getColor(8)},
+      {y:10, x:5, color: getColor(10)},
+      {y:12, x:6, color: getColor(12)},
+      {y:14, x:7, color: getColor(14)},
+      {y:16, x:8, color: getColor(16)},
+      {y:18, x:9, color: getColor(18)},
+      {y:20, x:10, color: getColor(20)},
+      {y:22, x:11, color: getColor(22)},
+      {y:24, x:12, color: getColor(24)},
+      {y:26, x:13, color: getColor(26)},
+      {y:28, x:14, color: getColor(28)},
+      {y:30, x:15, color: getColor(30)},
+      {y:32, x:16, color: getColor(32)},
+      {y:34, x:17, color: getColor(34)},
+      {y:36, x:18, color: getColor(36)},
+      {y:38, x:19, color: getColor(38)},
+      {y:40, x:20, color: getColor(40)},
+      {y:42, x:21, color: getColor(42)},
+      {y:44, x:22, color: getColor(44)},
+      {y:46, x:23, color: getColor(46)},
+      {y:48, x:24, color: getColor(48)},
+      {y:50, x:25, color: getColor(50)},
+      {y:52, x:26, color: getColor(52)},
+      {y:54, x:27, color: getColor(54)},
+      {y:56, x:28, color: getColor(56)},
+      {y:58, x:29, color: getColor(58)},
+      {y:60, x:30, color: getColor(60)},
+      {y:62, x:31, color: getColor(62)},
+      {y:64, x:32, color: getColor(64)},
+      {y:66, x:33, color: getColor(66)},
+      {y:68, x:34, color: getColor(68)},
+      {y:70, x:35, color: getColor(70)},
+      {y:72, x:36, color: getColor(72)},
+      {y:74, x:37, color: getColor(74)},
+      {y:76, x:38, color: getColor(76)},
+      {y:78, x:39, color: getColor(78)},
+      {y:80, x:40, color: getColor(80)},
+      {y:82, x:41, color: getColor(82)},
+      {y:84, x:42, color: getColor(84)},
+      {y:86, x:43, color: getColor(86)},
+      {y:88, x:44, color: getColor(88)},
+      {y:90, x:45, color: getColor(90)},
+      {y:92, x:46, color: getColor(92)},
+      {y:94, x:47, color: getColor(94)},
+      {y:96, x:48, color: getColor(96)},
+      {y:98, x:49, color: getColor(98)},
+      {y:96, x:50, color: getColor(96)},
+      {y:94, x:51, color: getColor(94)},
+      {y:92, x:52, color: getColor(92)},
+      {y:90, x:53, color: getColor(90)},
+      {y:88, x:54, color: getColor(88)},
+      {y:86, x:55, color: getColor(86)},
+      {y:84, x:56, color: getColor(84)},
+      {y:82, x:57, color: getColor(82)},
+      {y:80, x:58, color: getColor(80)},
+      {y:78, x:59, color: getColor(78)},
+    ],
   },
 ];
 
@@ -181,6 +126,38 @@ React.SFC<DashboardWidgetProps> = (props) => {
     margin,
   } = props;
 
+  const handleMouseMove = ( e: any ) => {
+    const pathElement = e.originalEvent.target;
+    const gElement = pathElement.parentNode;
+
+    // console.log('e:', e);
+    // const element = e.originalEvent.target;
+    // if ( e.originalEvent.target.style.fill !== 'transparent' ) {
+    console.log('pathElement:', pathElement);
+    console.log('gElement:', gElement);
+      // console.log('fill:', e.originalEvent.target.style.fill);
+    // }
+    
+
+    // console.log('e.scaleX', e.scaleX());
+    // const hint = document.createElement('div');
+    // const element = e.originalEvent.target;
+    // hint.style.width = '100px';
+    // hint.style.height = '50px';
+    // hint.style.backgroundColor = 'orange';
+    // e.originalEvent.target.style.fillOpacity = 1;
+    // element.appendChild(hint);
+  };
+
+  const handleMouseLeave = ( e: any ) => {
+    // const element = e.originalEvent.target;
+    // element.removeChild('div');
+  };
+
+  // const handleBarMove = ( target ) => {
+  //   console.log('bar:', target);
+  // }
+
 
   return (
     <Widget
@@ -191,37 +168,42 @@ React.SFC<DashboardWidgetProps> = (props) => {
         <WidgetHeader>{ widget_name }</WidgetHeader>
       </WidgetHeaderWrapper>
       <WidgetContent>
-          <Chart
-            series={series}
-            viewBox={'0 0 100 50'}
-            minY={0}
-            // layerWidth={50}
-            // scaleX={{paddingStart: 0, paddingEnd: .55}}
-            scaleY={{paddingTop: 2, paddingBottom: .05}}
-          >
-            <Layer width={'80%'} height={'100%'}>
-              <Ticks 
-                axis={'y'}
-                position={'right'}
-                lineLength={.95}
-                lineOffset={-1}
-                lineVisible={true}
-                lineStyle={{
-                  stroke: 'lightgray',
-                  strokeWidth: 0.2,
-                }}
-                labelStyle={{
-                  textAnchor: 'start',
-                  dominantBaseline: 'middle',
-                  fill: 'lightgray',
-                  fontSize: 2.5,
-                }}
-                labelAttributes={{x: -4}}
-              />
+        <Chart
+          series={series}
+          viewBox={'0 0 100 50'}
+          minY={0}
+          scaleX={{paddingStart: 1, paddingEnd: 0}}
+          scaleY={{paddingTop: 4, paddingBottom: .05}}
+        >
+          <Layer width={'100%'} height={'100%'}>
+            <Ticks 
+              axis={'y'}
+              position={'right'}
+              lineLength={.95}
+              lineOffset={-1}
+              lineVisible={true}
+              lineStyle={{
+                stroke: 'lightgray',
+                strokeWidth: 0.2,
+              }}
+              labelStyle={{
+                textAnchor: 'start',
+                dominantBaseline: 'middle',
+                fill: 'lightgray',
+                fontSize: 2.5,
+              }}
+              labelAttributes={{x: -4}}
+            />
+          </Layer>
+          <Layer width={'94%'} height={'100%'} position={'left bottom'}>
+            <Handlers
+              onMouseMove={handleMouseMove}
+              onMouseLeave={handleMouseLeave}
+              optimized={false}
+            >
               <Bars
-                // barWidth={.015}
-                innerPadding={.0001}
-                // groupPadding={.1}
+                barWidth={.015}
+                innerPadding={.01}
                 barStyle={{
                   fillOpacity: .5,
                   transition: 'all 250ms',
@@ -232,23 +214,39 @@ React.SFC<DashboardWidgetProps> = (props) => {
                   onMouseLeave: e => e.target.style.fillOpacity = .5,
                 }}
               />
-            </Layer>
-          </Chart>
-
+              <Title position={'left top'} style={{
+                textAnchor: 'middle',
+                fontSize: 2.5,
+              }}>
+                {'test'}
+              </Title>
+            </Handlers>
+          </Layer>
+        </Chart>
       </WidgetContent>
     </Widget>
   );
 }
+          // <Layer width={'100%'} height={'100%'}>
+          //   <rect x={0} y={0} width={20} height={10} rx={1} ry={1}></rect>
+          // </Layer>
+
+
               // <Labels 
               //   label={({point}) => (point.y)}
               //   dotStyle={{
-              //     textAnchor: 'middle',
-              //     alignmentBaseline: 'after-edge',
+              //     textAnchor: 'top',
+              //     dominantBaseline: 'text-after-edge',
               //     fontFamily: 'sans-serif',
-              //     fontSize: 1
+              //     fontSize: 2.5,
+              //     transition: 'all 250ms',
+              //     fillOpacity: 0,
               //   }}
               //   labelAttributes={{
-              //     y: 10
+              //     y: 0,
+              //     x: -1,
+              //     onMouseMove: e => e.target.style.fillOpacity = 1,
+              //     onMouseLeave: e => e.target.style.fillOpacity = 0,
               //   }}
               // />
 
