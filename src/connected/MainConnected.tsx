72e8c5e67764fe649ad830080bc78b71f4829139
Мельников Иван Игоревич <melnikov-ivan@mail.ru>
2018-04-30 03:50:04 +0300

    Fixing bugs in progress

--- src/connected/MainConnected.tsx
+++ src/connected/MainConnected.tsx
@@ -1,5 +1,5 @@
 import { bindActionCreators } from 'redux';
-import { connect, MapStateToProps } from 'react-redux';
+import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { Dispatch, RootState } from '@src/redux';
 import { withRouter } from 'react-router-dom';
